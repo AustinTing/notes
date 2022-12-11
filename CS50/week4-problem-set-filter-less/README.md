2a8a3d3fa9fe7eeb63a36e31e8073adfc958aa8e
CellBody <austintingwork@gmail.com>
2022-12-11 20:25:15 +0800

    CS50: add reflection in week4 filter less

--- CS50/week4-problem-set-filter-less/README.md
+++ CS50/week4-problem-set-filter-less/README.md
@@ -9,8 +9,20 @@ make filter --always-make
 
 ## Usage
 
+**g**rayscale
+
 ```sh
  ./filter -g ./images/tower.bmp ./outputs/tower.bmp
 ```
 
+**s**epia
 
+```sh
+ ./filter -s ./images/tower.bmp ./outputs/tower.bmp
+```
+
+**r**eflect
+
+```sh
+ ./filter -r ./images/tower.bmp ./outputs/tower.bmp
+```
--- CS50/week4-problem-set-filter-less/helpers.c
+++ CS50/week4-problem-set-filter-less/helpers.c
@@ -19,17 +19,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 }
 
 /**
- * @brief 
+ * @brief
  * 根據原三原色的初始值，算出需要的結果。大於 255 則只回傳 255 。
  * @param adjustRed - 要調整多少紅色。
  * @param adjustGreen - 要調整多少綠色。
  * @param adjustBlue - 要調整多少藍色。
  * @param pixel - RGBTRIPLE 的物件。
- * @return int 
+ * @return int
  */
 int calulate(float adjustRed, float adjustGreen, float adjustBlue, RGBTRIPLE pixel)
 {
-    int result = (adjustRed * pixel.rgbtRed + adjustGreen * pixel.rgbtGreen + adjustBlue  * pixel.rgbtBlue) + 0.5; // + 0.5 for rounding the digit
+    int result = (adjustRed * pixel.rgbtRed + adjustGreen * pixel.rgbtGreen + adjustBlue * pixel.rgbtBlue) + 0.5; // + 0.5 for rounding the digit
     return result > 255 ? 255 : result;
 }
 
@@ -43,7 +43,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         {
             RGBTRIPLE pixel = image[i][j];
             image[i][j].rgbtBlue = calulate(0.272, 0.534, 0.131, pixel);
-            image[i][j].rgbtGreen =calulate(0.349, 0.686, 0.168, pixel);
+            image[i][j].rgbtGreen = calulate(0.349, 0.686, 0.168, pixel);
             image[i][j].rgbtRed = calulate(0.393, 0.769, 0.189, pixel);
         }
     }
@@ -53,6 +53,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (int)(width / 2); j++)
+        {
+            RGBTRIPLE tempPixel = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = tempPixel;
+        }
+    }
     return;
 }
 
