e844062cedea9324b9cb319ff7651fad2feff2f1
CellBody <austintingwork@gmail.com>
2022-12-11 17:19:56 +0800

    CS50: add week4 problem set filter less

---
+++ CS50/week4-problem-set-filter-less/helpers.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            int avg = (int)((pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed) / 3.0 + 0.5);
+            printf("%i, %i, %i: %i\n", (int)pixel.rgbtBlue, (int)pixel.rgbtGreen, (int)pixel.rgbtRed, avg);
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtRed = avg;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
